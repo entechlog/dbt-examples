9d764ce35af80bad60c8ab6140fed1833bff145a
Siva Nadesan <entechlog@gmail.com>
2021-01-22 23:16:27 -0800

    Changes to refine the dbt and snowflake example using msql sakila db

--- mysql-sakila-db/dbt/models/prep/prep_sakila_language.sql
+++ mysql-sakila-db/dbt/models/prep/prep_sakila_language.sql
@@ -4,14 +4,16 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_sakila_rental') }}
+    select * from {{ ref('sakila_language') }}
 
 ),
 
 renamed as (
 
     select
-        *
+        language_id as language_id,
+        name as language_name,
+        last_update
 
     from source
 
