707970c91f604c47404dd786dde2d3f881e8e16f
entechlog <38563714+entechlog@users.noreply.github.com>
2021-06-10 00:02:01 -0700

    Revert "added dbt postgres demo (#1)" (#2)
    This reverts commit 011b3a538406af207d0f63f606474e4be3095dd8.

--- dbt-postgres/models/prep/movies_actors.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT mov.name
-	,act.first_name
-	,act.last_name
-	,act.gender
-FROM {{ref('movies')}} mov
-INNER JOIN {{ref('roles')}} rol ON rol.movie_id = mov.id
-INNER JOIN {{ref('actors')}} act ON act.id = rol.actor_id
