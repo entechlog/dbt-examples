d6ef50d1b5e45c7ba4f7001dc5bd943a2b3135c3
Siva Nadesan <entechlog@gmail.com>
2023-08-05 14:45:45 -0700

    changes to data the prd github action

--- dbt-docs/terraform/cloudfront-cognito/output.tf
+++ dbt-docs/terraform/cloudfront-cognito/output.tf
@@ -1,3 +1,7 @@
 output "cloudfront_distribution__domain_name" {
   value = module.cloudfront_cognito.cloudfront_distribution__domain_name
 }
+
+output "aws_s3_bucket__arn" {
+  value = module.cloudfront_cognito.aws_s3_bucket__arn
+}
